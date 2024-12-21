b08683843c338019e88b4dcdcfc86afbd6ed09c8
nwr27 <nanawartana27@gmail.com>
2024-12-21 21:30:29 +0700

    #7 url apps

--- DJANGO_TUTORIAL/blog/views.py
+++ DJANGO_TUTORIAL/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
   return render(request, 'blog.html')
+
+def recent(request):
+  return HttpResponse('ini recent post, kalau hanya ini recent maka yang muncul ini aja (ada apa dengan kata recent di HttpResponse)')
--- DJANGO_TUTORIAL/mywebsite/urls.py
+++ DJANGO_TUTORIAL/mywebsite/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
-from blog import views as blogViews 
 from contact import views as contactViews 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
     url(r'^about/$', views.about),
-    url(r'^blog/$', blogViews.index),
+    url(r'^blog/', include('blog.urls')),
     url(r'^contact/$', contactViews.index),
 
 ]
