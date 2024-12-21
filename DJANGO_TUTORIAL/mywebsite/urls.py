594399c9b1e593247e2d4bdb57a822cc9b5a1189
nwr27 <nanawartana27@gmail.com>
2024-12-21 21:10:53 +0700

    #6 apps

--- DJANGO_TUTORIAL/mywebsite/urls.py
+++ DJANGO_TUTORIAL/mywebsite/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+from blog import views as blogViews 
+from contact import views as contactViews 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
     url(r'^about/$', views.about),
+    url(r'^blog/$', blogViews.index),
+    url(r'^contact/$', contactViews.index),
 
 ]
